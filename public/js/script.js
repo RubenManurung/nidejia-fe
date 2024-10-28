f6a3dc8f381a5189e8272bbeae27a3e10b165141
RubenManurung <91777767+deveRuben@users.noreply.github.com>
2024-10-28 13:42:46 +0700

    fix navbar mobile

--- public/js/script.js
+++ public/js/script.js
@@ -39,3 +39,42 @@ const swiper = new Swiper(".fraction-slide-carousel", {
     }
   }
 });
+
+// Pilih checkbox dan elemen yang akan dimodifikasi
+const checkbox = document.getElementById('check-hamburger');
+const line1 = document.getElementById('line1');
+const line2 = document.getElementById('line2');
+const line3 = document.getElementById('line3');
+const navDefault = document.getElementById('navbar-default');
+
+// Tambahkan event listener untuk perubahan pada checkbox
+checkbox.addEventListener('change', function () {
+  // Jika checkbox dicentang, tambahkan class
+  if (checkbox.checked) {
+    line1.classList.add('rotate-45', 'translate-y-1.5');
+    line2.classList.add('opacity-0');
+    line3.classList.add('-rotate-45', '-translate-y-1.5');
+    navDefault.classList.remove('hidden')
+  } else {
+    // Jika checkbox tidak dicentang, hapus class
+    line1.classList.remove('rotate-45', 'translate-y-1.5');
+    line2.classList.remove('opacity-0');
+    line3.classList.remove('-rotate-45', '-translate-y-1.5');
+    navDefault.classList.add('hidden')
+  }
+});
+
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault(); // Prevent default anchor click behavior
+
+    // Get the target section based on the href value
+    const targetId = this.getAttribute('href').substring(1);
+    const targetSection = document.getElementById(targetId);
+
+    // Scroll to the target section with smooth behavior
+    targetSection.scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
